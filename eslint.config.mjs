948d0faf6705cb8e713949688529ee7212ac2e0c
NguyenThanhDuyOfficial <nguyenthanhduyofficial@gmail.com>
2026-09-14 14:29:03 +0700

    chore: change package name

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { libraryConfig } from '@odyssey/eslint-config/library';
+import { libraryConfig } from '@nguyenthanhduyofficial/eslint-config/library';
 import tsParser from '@typescript-eslint/parser';
 
 export default [
